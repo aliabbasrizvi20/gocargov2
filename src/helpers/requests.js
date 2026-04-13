80197ff04526bcf7187bbf36dba69bb4e36f7019
aliabbasrizvi20 <rockcodes0@gmail.com>
2026-04-13 12:45:05 +0530

    Changed Live URls

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = " https://drivo-vugc.onrender.com/";
 
 const INSTANCE = axios.create({
   baseURL: BASE_URL,
